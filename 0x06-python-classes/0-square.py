fb5f0cc7b7d83aa449f29b963cc8315a2e04d37e
Nezerwa <88612346+Nezerwa@users.noreply.github.com>
2022-10-17 07:43:43 +0200

    0x06. Python - Classes and Objects project

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Empty class that defines a square
+"""
+
+
+class Square:
+    """
+    Square definition
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Empty class that defines a square
+and initializes its size
+"""
+
+
+class Square:
+    """Square implementation
+    """
+
+    def __init__(self, size):
+        self.__size = size
