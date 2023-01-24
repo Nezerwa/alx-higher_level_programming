018590e75db6a42be9f7a9da7ecbb4fec9ccf590
Nezerwa <88612346+Nezerwa@users.noreply.github.com>
2023-01-24 14:53:15 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
+const filePath = process.argv[2];
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
