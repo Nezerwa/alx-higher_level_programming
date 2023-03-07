d78a0511137359d49e8013e2adb0527602340fdb
Nezerwa Eligrand <88612346+Nezerwa@users.noreply.github.com>
2023-03-07 15:57:55 +0200

    add project files

---
+++ 0x12-javascript-warm_up/0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const temp = [];
+      for (let j = 0; j < this.width; j++) temp.push('X');
+      console.log(`${temp.join('')}`);
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
